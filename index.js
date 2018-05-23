22b7a0564fd4dd423be77164ee1c906aae6c6252
Ivan Vodovozov <ivan.vodovozov@gmail.com>
2018-05-22 20:57:11 -0400

    multiple search added

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 // Get references to the tbody element, input field and button
 var $tbody = document.querySelector("tbody");
-var $dateInput = document.querySelector("#input");
+var $dateInput = document.querySelector("#inputDate");
+var $cityInput = document.querySelector("#inputCity");
+var $stateInput = document.querySelector("#inputState");
+var $shapeInput = document.querySelector("#inputShape");
 var $searchBtn = document.querySelector("#filter");
+var $resetBtn = document.querySelector("#reset");
+
 
 // Add an event listener to the searchButton, call handleSearchButtonClick when clicked
 $searchBtn.addEventListener("click", handleSearchButtonClick);
+$resetBtn.addEventListener("click", handleResetButtonClick);
 
 // Set filteredData to dataSet initially
 var filteredData = dataSet;
@@ -29,15 +35,32 @@ function renderTable() {
 
 function handleSearchButtonClick() {
   // Format the user's search by removing leading and trailing whitespace, lowercase the string
-  var filterDate = $dateInput.value.trim().toLowerCase();
-
+  var filterDate = $dateInput.value.trim();
+  var filterCity = $cityInput.value.trim().toLowerCase();
+  var filterState = $stateInput.value.trim().toLowerCase();
+  var filterShape = $shapeInput.value.trim().toLowerCase();
+  
   // Set filteredData to an array of all sightings whose datetime matches the filter
-  filteredData = dataSet.filter(function(curr_record) {
-    var sightingDate = curr_record.datetime;
+  filteredData = dataSet.filter(function (curr_record) {
     // If true, add the record to the filteredData, otherwise don't add it to filteredData
-    return sightingDate === filterDate;
+    var result=(!filterDate || curr_record.datetime == filterDate) &&
+              (!filterCity || curr_record.city == filterCity)&&
+              (!filterState || curr_record.state == filterState)&&
+              (!filterShape || curr_record.shape == filterShape);
+    return result;
   });
   renderTable();
+}
+
+
+function handleResetButtonClick() {
+// Empty the search fields
+$dateInput.value.value="";
+$cityInput.value="";
+$stateInput.value="";
+$shapeInput.value="";
+filteredData=dataSet;
+renderTable();
 }
 
 // Render the table for the first time on page load
